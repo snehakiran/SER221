1d97361ef9578a8152ec63689f174688e2a52811
Derek R. Castile <DRCastile@outlook.com>
2014-10-13 02:55:17 -0700

    finished program 1

---
+++ Assign1/pigGame.cpp
@@ -0,0 +1,39 @@
+//
+//  pigGame.cpp
+//  Assign1
+//
+//  Created by Derek R Castile on 10/13/14.
+//  Copyright (c) 2014 Derek R Castile. All rights reserved.
+//
+
+
+/*
+ 3. Pig game (50 points)
+ The game of Pig is a simple two player dice game in which the first player to reach 100 or more points wins.
+ Players take turns. On each turn a player rolls a six-sided die. After each roll:
+    • If the player rolls a 2-6 then he can either: • ROLL AGAIN or
+    • HOLD. At this point the sum of all rolls made this turn is added to the player’s total score and it becomes the other player’s turn.
+    • If the player rolls a 1 then the player loses his turn. He gets no new points and it becomes the opponent’s turn.
+ If a player reaches 100 or more points after holding then the player wins.
+ 
+ Write a program that plays the game of Pig, where one player is a human and the other is the computer.
+    Allow the human to input ‘r’ (or ‘R’) to roll again or ‘h’ (or ‘H’) to hold.
+    The computer program should play according to the following rule: Keep rolling on the computer’s turn until it has accumulated 20 or more points, then hold. Of course, if the computer wins or rolls a 1 then the turn ends immediately. Allow the human to roll first.
+ 
+    Write your program using at least two functions:
+        - int humanTurn(int humanTotalScore);
+        - int computerTurn(int computerTotalScore);
+    These functions should perform the necessary logic to handle a single turn for either the com- puter or the human. The input parameter is the total score for the human or computer. The functions should return the turn total to be added to the total score upon completion of the turn. For example, if the human rolls a 3 and 6 and then holds, then humanTurn should re- turn 9. However, if the human rolls a 3 and 6 and then a 1, then the function should return 0.
+    
+ HINTS:
+        Note that the description of the humanTurn and computerTurn functions indicate that an outside variable must keep track of the total score by adding to itself the value re- turned by the functions at the completion of each player’s turn.
+        Also, to implement roll of a dice you can use the srand(int) and rand() functions from <cstdlib>.
+        A random num- ber between M and N (both inclusive, where M < N) is equal to ((rand() % (N-M+1)) + M).
+ */
+
+#include <iostream>
+
+
+int main() {
+    
+}
--- Assign1/rps.cpp
+++ Assign1/rps.cpp
@@ -6,4 +6,35 @@
 //  Copyright (c) 2014 DRC Ventures. All rights reserved.
 //
 
-#include <stdio.h>
+
+
+/*
+ 2. Rock, Paper, Scissor (30 points)
+Write a program to score the paper-rock-scissor game. Each of the two players type in either P, R, or S.
+     The program then announces the winner based on the rule: 
+        Paper covers Rock, Rock breaks Scissors, Scissors cut Paper, or Nobody wins.
+     Be sure to allow the players to use lowercase as well as uppercase letters.
+     Your program should allow the users to play repeatedly until they say they are done.
+     Your program should also keep a score for each player and display that after each play.
+        
+        Sample run:
+            > Player 1: Please enter either (R)ock, (P)aper, or (S)cissors: P
+            > Player 2: Please enter either (R)ock, (P)aper, or (S)cissors: s
+            > Player 2 wins.
+            > Scores after this play:
+            > Player 1: 0
+            > Player 2: 1
+            > Thanks!!
+            > Play again? Y/y continues, other quits: n
+ */
+
+
+
+#include <iostream>
+
+
+
+
+int main() {
+
+}
